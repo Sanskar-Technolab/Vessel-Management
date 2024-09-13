e8c7ecf24a9344150298e234c631c1bc51111456
darshitvadadoriya <darshit@sanskartechnolab.com>
2024-09-13 18:28:22 +0530

    update printformat and add print icon

--- vessel/public/js/reports/printpreview.js
+++ vessel/public/js/reports/printpreview.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
+    console.log(typeof(localStorage.getItem("party_name")));
+    if(localStorage.getItem("party_name") && localStorage.getItem("party_name")!= "undefined"){
+        $("#party").html(localStorage.getItem("party_name"))
+    }
+    else{
+        $("#party_section").hide()
+    }
+   
     
- //  get today date in posting date
- var today = new Date();
- var day = String(today.getDate()).padStart(2, '0');
- var month = String(today.getMonth() + 1).padStart(2, '0');
- var year = today.getFullYear();
- var files = []
+    var from_date = localStorage.getItem("min_date").replaceAll("/","-")
+    var to_date = localStorage.getItem("max_date").replaceAll("/","-")
+    var party_name = localStorage.getItem("party_name")
+    // console.log(party_name);
+   
+    $("#from_date").html(from_date)
+    $("#to_date").html(to_date)
 
- 
- //format year-month-date
- var today_date = day + '-' + month + '-' + year;
- $('#statement_date').text(today_date); //set today date in statement date
- 
-    console.log($("#statement_date"));
+   
 
 })
+
+
+
+
